a18f256c959fe7ebeca5f14b28d13bd0911008b8
Emma Ritcey <emritcey@gmail.com>
2016-06-01 11:20:35 -0500

    Update 20160601104748_create_questionns_table.rb

--- db/migrate/20160601104748_create_questionns_table.rb
+++ db/migrate/20160601104748_create_questionns_table.rb
@@ -4,9 +4,10 @@ class CreateQuestionnsTable < ActiveRecord::Migration
   		t.string :title
   		t.string :description
   		t.integer :user_id
-  		t.integer :fav_answer_id
+  		t.integer :favorite_answer_id
   		t.integer :views_count
 
   		t.timestamps null: false
+  	end
   end
 end
