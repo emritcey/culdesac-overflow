c85a8b9c80c0d96b8312c7a3f51fc82921338324
Emma Ritcey <emritcey@gmail.com>
2016-06-01 11:05:56 -0500

    Update comment.rb

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,7 @@
 class Comment < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :commentable, polymorphic: true 
-  validates :description, presence: true
+  belongs_to :commentable, polymorphic: true
+  belongs_to :commenter, class_name: "User"
+  validates :description, :user_id, presence: true
 end
 
