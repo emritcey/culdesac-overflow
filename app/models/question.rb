e0efb4da84295e5189eb2f1f1f2cc624dc9c88f2
Emma Ritcey <email": null>
2016-06-01 14:00:55 -0500

    add two files for index and some css

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,12 +2,13 @@ class Question < ActiveRecord::Base
   # Remember to create a migration!
   belongs_to :questioner
   belongs_to :favorite_answer, class_name: "Answer"
-  
+
+  has_many :votes, as: :votable
   has_many :answers
   has_many :comments, as: :commentable
   has_many :taggings
   has_many :tags, through: :taggings
-  
-  validates :title, :description, :user_id, presence: true 
+
+  validates :title, :description, :user_id, presence: true
 
 end
