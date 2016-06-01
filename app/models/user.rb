2a5c2acb8f36da1ef7d33218fe6fb0bc044d45e4
Emma Ritcey <emritcey@gmail.com>
2016-06-01 11:08:16 -0500

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ActiveRecord::Base
   has_many :comments
   has_many :votes
 
-  validates :username, presence: :true, uniqueness: :true
-  validates :password, presence: :true
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 end
