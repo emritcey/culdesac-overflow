358ac7cb326d07ef08f27917d559edf0941d16ac
Emma Ritcey <emritcey@gmail.com>
2016-06-01 11:04:39 -0500

    Update answer.rb

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :user
+  belongs_to :answerer, class_name: "User"
   belongs_to :question
   has_many   :comments, as: :commentable
   has_many   :votes, as: :votable 
