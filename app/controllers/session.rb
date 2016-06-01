a95e2cdb17c57353cb2198ceb70dfcd06cca00be
Theron Cross <theroncross@gmail.com>
2016-06-01 15:30:21 -0500

    Add controller and view for logging in

--- app/controllers/session.rb
+++ app/controllers/session.rb
@@ -1,19 +1,19 @@
-get '/login' do
-  erb :'/login'
+get '/users/login' do
+  erb :'/users/login'
 end
 
-post '/login' do
+post '/users/login' do
   @user = User.find_by(username: params[:username])
   if @user.authenticate(params[:password])
     session[:user_id] = @user.id
     redirect "/"
   else
     @errors = "Invalid username or password"
-    erb :'/login'
+    erb :'/users/login'
   end
 end
 
-get '/logout' do
+get '/users/logout' do
   session.delete(:user_id)
   redirect '/'
 end
