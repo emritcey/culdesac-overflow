bcb2b3b997c141b2fb67631915d21d301460c229
okjill <okjill@gmail.com>
2016-06-01 16:10:58 -0500

    fix username display in display view

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :questioner
+  belongs_to :questioner, class_name: "User", foreign_key: :user_id
   belongs_to :favorite_answer, class_name: "Answer"
 
   has_many :votes, as: :votable
