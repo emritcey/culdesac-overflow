390ce45506b6e56ba6c3500a428b215ea7248063
Theron Cross <theroncross@gmail.com>
2016-06-01 10:47:02 -0500

    Add user model with validations

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  has_many :questions
+  has_many :answers
+  has_many :comments
+  has_many :votes
+
+  validates :username, presence: :true, uniqueness: :true
+  validates :password, presence: :true
+end
