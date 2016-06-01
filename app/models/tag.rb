53a072109eee600acb53a1c895c36d83961d99fe
Emma Ritcey <email": null>
2016-06-01 10:46:57 -0500

    add validations

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,4 +2,7 @@ class Tag < ActiveRecord::Base
   # Remember to create a migration!
   has_many :taggings
   has_many :questions, through: :taggings
+
+  validates_uniqueness_of :description
+  validates :description, presence: true
 end
