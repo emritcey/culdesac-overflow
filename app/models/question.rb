35d939167495462f4fcf6761de292a9ecfd671d8
Emma Ritcey <emritcey@gmail.com>
2016-06-01 11:19:42 -0500

    Update question.rb

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,11 +1,13 @@
 class Question < ActiveRecord::Base
   # Remember to create a migration!
-  belongs_to :user
-  belongs_to :answer
-
+  belongs_to :questioner
+  belongs_to :favorite_answer, class_name: "Answer"
+  
+  has_many :answers
   has_many :comments, as: :commentable
   has_many :taggings
+  has_many :tags, through: :taggings
   
-  validates :title, :description, presence: true 
+  validates :title, :description, :user_id, presence: true 
 
 end
