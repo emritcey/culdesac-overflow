75576d7efb5e73ca3528d139309bd1b1fae66c50
okjill <okjill@gmail.com>
2016-06-01 22:07:52 -0500

    user profile view and logic

---
+++ app/controllers/user.rb
@@ -0,0 +1,58 @@
+enable :sessions
+
+get '/session-viewer' do 
+  session.inspect
+end
+
+get '/users' do
+  erb :'users/index'
+end
+
+
+get '/users/new' do 
+  erb :'users/new'
+end
+
+post '/users' do
+  user = User.new(params[:user])
+
+  if user.save
+    redirect '/users/login' # will want to automatically sign user in after registration
+  else
+    @errors = user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/users/login' do 
+  erb :'users/login'
+end
+
+get '/users/:user_id' do
+  @user = User.find_by(id: params[:user_id])
+  erb :'users/show'
+end
+
+get '/users/:user_id/answers' do
+  @user = User.find_by(id: params[:user_id])
+  @answers = @user.answers
+  erb :'users/show_answers'
+end
+
+get '/users/:user_id/questions' do
+  @user = User.find_by(id: params[:user_id])
+  @questions = @user.questions
+  erb :'users/show_questions'
+end
+
+get '/users/:user_id/votes' do
+  @user = User.find_by(id: params[:user_id])
+  @votes = @user.votes
+  erb :'users/show_votes'
+end
+
+delete '/users/logout' do 
+  session.clear
+  erb :'users/logout'
+end
+
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -1,3 +1,12 @@
 class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
+
+  def get_votable_object
+    if votable_type == "Answer"
+      answer_text = Answer.find_by(id: votable_id).description
+      return answer_text.split[0...10].join(' ').concat("...")
+    elsif votable_type == "Question"
+      return Question.find_by(id: votable_id).title
+    end
+  end
 end
